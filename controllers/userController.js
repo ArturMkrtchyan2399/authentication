7d07724b81171d6390c2a2652e9971b2a479b745
ArturMkrt <artur-mkrtchyan-2019@list.ru>
2021-02-08 15:04:15 +0400

    Completion Commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const register = async (req, res) => {
     try {
         const { fullName, email, password } = req.body;
-        let errors = [];
+        const errors = [];
         const nameRegExp = "[A-Z][a-z]+\\s+[A-Z][a-z]+";
         const mailRegExp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])";
         const passRegExp = ["^(.*?[A-Z]){2,}.*$", "^(.*?\\d){2,}.*$"];
